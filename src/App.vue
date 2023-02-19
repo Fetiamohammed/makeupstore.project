eced8e74153f44fa640aa72fe65ebb372885d8c9
Fetiamohammed <fetia-awol@hotmail.com>
2023-02-19 02:42:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavBar from "./components/NavBar.vue";
+import FooterFooter from "./components/FooterFooter.vue";
+//import FotterFotter from "./components/FotterFotter.vue";
+</script>
+
+<template>
+  <NavBar />
+  <FooterFooter />
+</template>
+
+<style scoped></style>
